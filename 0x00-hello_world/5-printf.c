ed3efe28077b9be5db35cba1b69bae97d73bd7ce
Chinwe-Collins <collinschinweesther@gmail.com>
2023-06-16 15:29:01 +0000

    Task 5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - Entry Point
+ *
+ * Return: return 0 if the program is successful
+ */
+ 
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+
+	return (0);
+}
