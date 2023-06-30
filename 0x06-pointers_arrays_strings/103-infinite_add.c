66eab71a6847cdf91716e766e013c55bec5ac61a
Chinwe-Collins <collinschinweesther@gmail.com>
2023-06-30 10:08:46 +0000

    Task 103

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -19,7 +19,7 @@ void rev_string(char *n)
 	for (j = 0; j < i; j++, i--)
 	{
 	temp = *(n + j);
-	(n + j) = *(n + i);
+	*(n + j) = *(n + i);
 
 	*(n + i) = temp;													}
 }
@@ -36,6 +36,7 @@ char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
 	int overflow = 0, i = 0, j = 0, digits = 0;
 	int val1 = 0, val2 = 0, temp_tot = 0;
+
 	while (*(n1 + i) != '\0')
 	i++;
 	while (*(n2 + j) != '\0')
