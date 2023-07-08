85db47b424470a8085203e12cae062d309c79c73
Chinwe <collinschinweesther@gmail.com>
2023-07-08 04:32:45 -0700

    Task 1

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 if successful
+ */
+int main(int argc, char *argv[])
+{
+	printf("%d\n", argc - 1);
+	(void) argv;
+
+	return (0);
+}
