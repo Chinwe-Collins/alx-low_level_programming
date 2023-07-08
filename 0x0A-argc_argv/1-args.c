acdcf909570fbb2a4c7b1aa43df167e59b27f1a6
Chinwe <collinschinweesther@gmail.com>
2023-07-08 05:36:22 -0700

    Task 1

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -2,7 +2,8 @@
 #include "main.h"
 /**
  * main - Entry point
- *
+ * @argc: counting the number
+ * @argv: array of argument
  * Return: Always 0 if successful
  */
 int main(int argc, char *argv[])
