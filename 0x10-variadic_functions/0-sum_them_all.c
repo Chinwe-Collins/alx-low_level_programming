3013dce0be07eb62cbd89cc20c8a89e41472a478
Chinwe <collinschinweesther@gmail.com>
2023-07-21 04:54:52 -0700

    Task 0

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,28 @@
+#include "variadic_functions.h"
+#include <stdarg.h>
+#include <stdio.h>
+/**
+ * sum_them_all - function that sums all the input integers
+ * @n: input to the function
+ */
+int sum_them_all(const unsigned int n, ...)
+{
+	va_list add;
+	unsigned int i;
+	int result;
+
+	if (n == 0)
+		return (0);
+
+	va_start(add, n);
+
+	result = 0;
+
+	for (i = 0; i < n; i++)
+	{
+		result +=  va_arg(add, int);
+	}
+
+	va_end(add);
+	return (result);
+}
