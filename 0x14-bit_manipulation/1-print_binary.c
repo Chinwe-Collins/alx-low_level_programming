db65819f792a33b5718337fde055fda13a0ac1bc
Chinwe-Collins <collinschinweesther@gmail.com>
2023-08-05 10:57:11 +0000

    Task 0

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * print_binary - function that prints binary 
+ * @n: input to the function
+ * Retun: returns nothing if successful
+ */
+void print_binary(unsigned long int n)
+{
+
+}
--- 0x14-bit_manipulation/pract.c
+++ 0x14-bit_manipulation/pract.c
@@ -1,28 +1,25 @@
 #include <stdio.h>
 #include <string.h>
 #include "main.h"
-/**
- * binary_to_uint - function that converts string to integer
- * @b: input to the function
- * Return: returns unsigned int if successful
- */
 unsigned int binary_to_uint(const char *b)
 {
 	unsigned int result = 0;
 	int i, j;
 	int len;
-
+	
 	len = strlen(b);
 	for (i = len - 1, j = 0; i >= 0; i--, j++)
 	{
 		if (b[j]  == '1')
 		{
-			result = result + (1 << i);
+			result = result + (1 << i); 
 		}
-		else if (b[j] != '0')												                {
+		else if (b[j] != '0')
+		{
 			return (0);
 		}
+
 	}
 
-	return (result);
+		return (result);
 }
