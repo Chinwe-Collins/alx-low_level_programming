f1a5d30fb32fe51f95d89adab55509c2bc7d77ae
Chinwe-Collins <collinschinweesther@gmail.com>
2023-08-11 20:40:13 +0000

    Finished

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -12,12 +12,12 @@ void close_file(int fd);
 char *create_buffer(char *file)
 {
 	char *buffer;
-	
+
 	buffer = malloc(sizeof(char) * 1024);
-	
+
 	if (buffer == NULL)
 	{
-		dprintf(STDERR_FILENO,"Error: Can't write to %s\n", file);
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", file);
 		exit(99);
 	}
 	return (buffer);
@@ -29,9 +29,9 @@ char *create_buffer(char *file)
 void close_file(int fd)
 {
 	int c;
-	
+
 	c = close(fd);
-	
+
 	if (c == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd);
@@ -54,19 +54,18 @@ int main(int argc, char *argv[])
 {
 	int from, to, r, w;
 	char *buffer;
-	
+
 	if (argc != 3)
 	{
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 		exit(97);
 	}
-	
+
 	buffer = create_buffer(argv[2]);
 	from = open(argv[1], O_RDONLY);
 	r = read(from, buffer, 1024);
 	to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
-	do 
-	{
+	do {
 		if (from == -1 || r == -1)
 		{
 			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
@@ -82,12 +81,11 @@ int main(int argc, char *argv[])
 		}
 		r = read(from, buffer, 1024);
 		to = open(argv[2], O_WRONLY | O_APPEND);
-	}
-	while (r > 0);
+	} while (r > 0);
 	free(buffer);
 	close_file(from);
 	close_file(to);
-	
+
 	return (0);
 }
 
