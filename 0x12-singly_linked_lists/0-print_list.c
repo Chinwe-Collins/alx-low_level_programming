1010284e9c2eec4d895f639cefb6725ba57ab1a4
Chinwe-Collins <collinschinweesther@gmail.com>
2023-08-11 07:20:55 +0000

    Task 0

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -7,21 +7,17 @@
  */
 size_t print_list(const list_t *h)
 {
-	size_t i = 0;
-
-	while (h != NULL)
+	size_t s = 0;
+	
+	while (h)
 	{
-		if (h->str == NULL)
-		{
+		if (!h->str)
 			printf("[0] (nil)\n");
-		}
 		else
-		{
-			printf("[%lu] %s\n", i, h->str);
-		}
-		i++;
+			printf("[%u] %s\n", h->len, h->str);
 		h = h->next;
+		s++;
 	}
-
-	return (i);
+	return (s);
 }
+
